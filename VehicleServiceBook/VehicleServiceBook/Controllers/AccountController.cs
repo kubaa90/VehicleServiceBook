7674dba4d72637b4950f754126d71ba9221a352f
Jakub Adamczyk <jakub.adamczyk1@gmail.com>
2020-10-20 18:45:31 +0200

    added layout, configuration and startup

---
+++ VehicleServiceBook/VehicleServiceBook/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VehicleServiceBook.Controllers
+{
+    public class AccountController:Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(SignInManager<IdentityUser> signInManager,
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            /*if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else*/
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ VehicleServiceBook/VehicleServiceBook/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VehicleServiceBook.ViewModels
+{
+    public class RegisterViewModel
+    {
+        public int ID { get; set; }
+        [Required]
+        [Display(Name = "Imię")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Nazwisko")]
+        public string Surname { get; set; }
+        public string UserName { get; set; }
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string? Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Powtórz hasło")]
+        public string RepeatPassword { get; set; }
+    }
+}
